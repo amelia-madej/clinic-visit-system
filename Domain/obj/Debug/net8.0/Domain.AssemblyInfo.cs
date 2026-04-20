6492e2b4c872f9b4c85e6f4c9c7f8ed360083473
Justyna Sarkowicz <justyna.sarkowicz@elte-s.com>
2026-04-20 19:31:16 +0200

    Add projects

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7659e26e2f3ab706f404d2f0e03d6a4dcd273218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c72cb64011b094fc75a9676a187492951235b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
