2fc40863534431578717f696959f13e3ba0791a0
Justyna Sarkowicz <justyna.sarkowicz@elte-s.com>
2026-05-12 18:08:51 +0200

    added BlazorClient

---
+++ BlazorClient/Program.cs
@@ -0,0 +1,24 @@
+using BlazorClient;
+using BlazorClient.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddScoped<AppStateService>();
+
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration.GetValue<string>("ClientVisitAPIUrl"))
+});
+
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
---
+++ BlazorClient/Services/AppStateService.cs
@@ -0,0 +1,17 @@
+﻿using SharedKernel.DTOs;
+
+namespace BlazorClient.Services
+{
+    public class AppStateService
+    {
+        public PatientListItemDto? CurrentPatient { get; private set; }
+
+        public event Action? OnChange;
+
+        public void SetPatient(PatientListItemDto? patient)
+        {
+            CurrentPatient = patient;
+            OnChange?.Invoke();
+        }
+    }
+}
